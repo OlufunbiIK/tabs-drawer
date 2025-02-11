c99df0eeff435772d9e2487b751782cf7ec44160
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-02-11 14:16:43 +0100

    Still on drawes

--- app/(drawers)/CustomDrawerContent.tsx
+++ app/(drawers)/CustomDrawerContent.tsx
@@ -1,23 +1,52 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
-import { router } from "expo-router";
+import { router, usePathname } from "expo-router";
+import { Feather } from "@expo/vector-icons";
 
 const CustomDrawerContent = (props: any) => {
+  const pathname = usePathname();
+
+  const isActiveRoute = (routePath: string) => {
+    return pathname === routePath;
+  };
   return (
     <View style={{ flex: 1 }}>
-      <DrawerContentScrollView {...props} scrollEnabled={false}>
+      <DrawerContentScrollView
+        {...props}
+        scrollEnabled={false}
+        contentContainerStyle={{ backgroundColor: "#dda", height: "100%" }}
+      >
         <DrawerItem
           label={"Home"}
           onPress={() => router.push("/(drawers)/(tabs)")}
+          labelStyle={{
+            color: isActiveRoute("/(drawers)/(tabs)") ? "red" : "black",
+          }}
+          icon={({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          )}
         />
+
         <DrawerItem
           label={"Services"}
           onPress={() => router.push("/(drawers)/(tabs)/services")}
+          labelStyle={{
+            color: isActiveRoute("/(drawers)/(tabs)") ? "red" : "black",
+          }}
+          icon={({ color, size }) => (
+            <Feather name="briefcase" size={24} color={color} />
+          )}
         />
         <DrawerItem
           label={"Profile"}
           onPress={() => router.push("/(drawers)/(tabs)/profile")}
+          labelStyle={{
+            color: isActiveRoute("/(drawers)/(tabs)") ? "red" : "black",
+          }}
+          icon={({ color, size }) => (
+            <Feather name="user" size={24} color={color} />
+          )}
         />
       </DrawerContentScrollView>
     </View>
